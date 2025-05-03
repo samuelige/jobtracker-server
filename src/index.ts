bcf47957ba3e20ed4391982d9ef8150c7c8b9889
Samuel Ige <samuelige.iyanu@gmail.com>
2025-05-04 00:52:49 +0100

    add: db url

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
 import express, { Express } from "express"
+import JobTrackerServer from "./server";
+import { databaseConnect } from "./server/database";
 const initializeApp = (): void => {
-  const app: Express = express()
+  const app: Express = express();
+  const jobtrackerServer = new JobTrackerServer(app);
+  databaseConnect().then(()=>{
+    jobtrackerServer.start();
+  })
 };
+
+initializeApp();
